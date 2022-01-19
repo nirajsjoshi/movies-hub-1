209179a2494b0ef5c7d5609c78d8ec4fa0c4ab75
nikhill56 <61156011+nikhill56@users.noreply.github.com>
2022-01-19 12:39:49 +0530

    signin

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
-import React from 'react';
+import React from "react";
 
-//Routing 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+//Routing
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 //Components
-import Header from './components/Header';
-import Home from './components/Home';
-import Movie from './components/Movie';
-import NotFound from './components/NotFound';
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Movie from "./components/Movie";
+import NotFound from "./components/NotFound";
 //aws
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-import {withAuthenticator} from '@aws-amplify/ui-react';
-import {GlobalStyle} from './GlobalStyle';
-
-Amplify.configure(config);
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { GlobalStyle } from "./GlobalStyle";
+//import "@aws-amplify/ui-react.styles.css";
+//Amplify.configure(awsconfig);
 
 //Styles
 
-
-const App= ()=> {
+const App = () => {
   return (
     <Router>
-      <Header/>
-
+      <Header />
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/:movieId' element={<Movie/>}/>
-        <Route path='/*' element={<NotFound/>}/>
-      </Routes>  
-      <GlobalStyle/>
+        <Route path="/" element={<Home />} />
+        <Route path="/:movieId" element={<Movie />} />
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
+      <GlobalStyle />
     </Router>
   );
 };
 
-export default withAuthenticator(App);
+export default App;
