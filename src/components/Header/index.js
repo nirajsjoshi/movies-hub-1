bedd6a7328bc91e66300f9d349ed57b4f8cb3ec4
nikhill56 <61156011+nikhill56@users.noreply.github.com>
2022-01-19 23:05:07 +0530

    added default

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import RMDBLogo from '../../images/react-movie-logo.svg';
-import TMDBLogo from '../../images/tmdb_logo.svg';
+import RMDBLogo from "../../images/react-movie-logo.svg";
+import TMDBLogo from "../../images/tmdb_logo.svg";
 
-import {Wrapper , Content, LogoImg, TMDBLogoImg } from './Header.styles';
+import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { useLocation } from "react-router-dom";
 
-const Header = () =>{
-    return (
+const Header = () => {
+  const location = useLocation();
+  console.log(location.pathname);
+  return (
     <Wrapper>
-        <Content>
-            <Link to="/">
-                <LogoImg src={RMDBLogo} alt="rmdb-logo"/>
-            </Link>
-            <LogoImg src={RMDBLogo} alt="rmdb-logo"/>
-            <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo"/>
-        </Content>
+      <Content>
+        <Link to="/">
+          <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+        </Link>
+        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+        <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+        {location.pathname !== "/" && <button>Sign Out</button>}
+      </Content>
     </Wrapper>
-    )
+  );
 };
 
 export default Header;
