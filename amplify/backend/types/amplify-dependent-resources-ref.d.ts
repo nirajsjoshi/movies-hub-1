23ab6bbc249636caf56715a4ebe7adf39d5db25f
nikhill56 <61156011+nikhill56@users.noreply.github.com>
2022-01-20 20:04:06 +0530

    prefinal signout

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
     "auth": {
-        "movieshubnewae152f98": {
+        "movieshubv2806e9b91": {
             "IdentityPoolId": "string",
             "IdentityPoolName": "string",
             "UserPoolId": "string",
